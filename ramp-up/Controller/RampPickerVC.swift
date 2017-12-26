4129f89419956b4943e3795808b0c4fae4a824d1
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-26 10:37:34 -0800

    Added new file to Controller.  Created class RampPickerVC.  General code cleanup.

---
+++ ramp-up/Controller/RampPickerVC.swift
@@ -0,0 +1,29 @@
+//
+//  RampPickerVC.swift
+//  ramp-up
+//
+//  Created by Donald Belliveau on 2017-12-26.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class RampPickerVC: UIViewController {
+    
+    // Functions
+    
+    // View Did Load.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    } // END View Did Load.
+    
+    
+    // Did Receive Memory Warning.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    } // END Did Receive Memory Warning.
+    
+    
+} // END Class.
