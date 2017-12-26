d0b57da70a443ee870550048a6a178e35ddc7343
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-26 10:44:17 -0800

    Added Instance Variables along with an Init.

--- ramp-up/Controller/RampPickerVC.swift
+++ ramp-up/Controller/RampPickerVC.swift
@@ -11,11 +11,30 @@ import SceneKit
 
 class RampPickerVC: UIViewController {
     
+    // Instance Variables
+    
+    var sceneView: SCNView!
+    var size: CGSize!
+    
+    // Initializer
+    
+    init(size: CGSize) {
+        super.init(nibName: nil, bundle: nil)
+        self.size = size
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // Functions
     
     // View Did Load.
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
     } // END View Did Load.
     
     
